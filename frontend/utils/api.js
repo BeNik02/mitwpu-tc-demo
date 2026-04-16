0fb498b0e9063a2a6cb6c277338dddbcb3047ce6
BeNik02 <nikhil.bawaskar02@gmail.com>
2026-04-16 10:12:30 +0530

    update API URL to Render deployment

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000/api';
+const BASE_URL = 'https://mitwpu-tc-demo.onrender.com/api';
 
 const getToken = () => localStorage.getItem('token');
 
